ab7efc4d6055b0b8ce5b49d506b91d921ac6d1d7
Wajahat Hussain <wajahat.hussain@apple1.local>
2020-03-12 21:25:37 +0500

    fixes

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 const _ = require("lodash");
+const { Op } = require("sequelize");
+const { QueryTypes } = require('sequelize');
+
+
 const { Products } = require('./Models/products')
 const { Category } = require('./Models/category')
 const {Product_Category}=require('./Models/product_category')
 const {Product_Attributes} = require('./Models/product_attribute')
+const {sequelize}=require('./shared/dbConnection')
 
 app.get('/products', async (req, res) => {
     let final=[];
-    if(!req.query.sort && !req.query.filter){
+    if(!req.query.sort && !req.query.gender && !req.query.size && !req.query.color){
     const products = await Products.findAll();
     res.send(products);
     }
-    else if (req.query.sort && !req.query.filter){
+    else if (req.query.sort && !req.query.gender && !req.query.size && !req.query.color){
         if(req.query.sort==="DESC"){
         const products = await Products.findAll({
             order:[['price','DESC']]
@@ -27,11 +32,11 @@ app.get('/products', async (req, res) => {
     }
     }
     else {
-        await Product_Attributes.findAll(
-            {attributes:['product_id'],where:{
-                attribute_value_id:parseInt(req.query.filter)
-            }}
-        ).map(async e=>{
+        let f1=parseInt(req.query.gender)
+        let f2=parseInt(req.query.size);
+        let f3=parseInt(req.query.color);
+        let query='SELECT a.product_id FROM product_attribute AS a JOIN product_attribute AS b ON a.product_id=b.product_id JOIN product_attribute AS c ON b.product_id=c.product_id WHERE a.attribute_value_id = 1 AND b.attribute_value_id = 1 AND c.attribute_value_id = 16'
+        let res= await sequelize.query(query,{ type: QueryTypes.SELECT }).map(async e=>{
            final.push(await Products.findOne({where:{product_id: e.dataValues.product_id}}));     
            });
            if(req.query.sort){
