89c54129693d11e6e163713c68cd5a24f569f501
Wajahat Hussain <wajahat.hussain@northbaysolutions.net>
2020-03-23 16:43:24 +0500

    orders and ordered products api completed

--- backend/controllers/ordersController.js
+++ backend/controllers/ordersController.js
@@ -1,11 +1,18 @@
 const orders = require('../models').orders;
+const ordered_products = require('../models').ordered_products;
 
-
-addOrder = async (customer_id, product_id, size, color, quantity, order_date, total_price) => {
+createOrder = async (customer_id, order_date, total_price) => {
     const order = await orders.create({
-        customer_id, product_id, size, color, quantity, order_date, total_price
+        customer_id, order_date, total_price
     })
     return order;
 }
 
-module.exports = { addOrder }
+addOrderedProducts = async (order_id, product_id, size, color, quantity,total_price) => {
+    const order = await ordered_products.create({
+        order_id, product_id, size, color, quantity,total_price
+    })
+    return order;
+}
+
+module.exports = { createOrder, addOrderedProducts }
--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
-const{ addOrder}= require('../controllers/ordersController');
+const{ createOrder,addOrderedProducts }= require('../controllers/ordersController');
 
 router.post('/', async (req, res) => {
-    let order = await addOrder(req.body.customer_id, req.body.product_id, req.body.size, req.body.color, req.body.quantity, req.body.order_date, req.body.total_price)
+    let order = await createOrder(req.body.customer_id, req.body.order_date, req.body.total_price)
     res.send(order);
 });
-
+router.post('/:id', async (req, res) => {
+    const order_id = req.params.id;
+    let order = await addOrderedProducts(order_id, req.body.product_id, req.body.size, req.body.color,
+        req.body.quantity,req.body.total_price)
+    res.send(order);
+});
 module.exports = router;
